5ef367be11f88e754c63235116a5e272d4e14e42
govin07 <govindrajput02653@gmail.com>
2024-01-22 08:42:39 +0530

    react

---
+++ 11project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Govind singh solanki</h1>
+    </>
+  )
+}
+
+export default App
