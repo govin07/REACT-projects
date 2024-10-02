c8d077de33cf6986d2337d854ce9104e7858cb99
govin07 <govindrajput02653@gmail.com>
2024-10-02 20:46:18 +0530

    React-Revision

---
+++ 13project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Name from './Components/Name'
+import './App.css'
+
+function App() {
+  
+
+
+  return (
+    <>
+    <h1>govind singh solanki</h1>
+    <Name/>
+     
+     
+    </>
+  )
+}
+
+export default App
---
+++ 13project/src/Components/Name.jsx
@@ -0,0 +1,9 @@
+import './Name.css'
+
+function Name(){
+    return(
+        <h2>Aditya singh Tawar</h2>
+    )
+}
+
+export default Name
